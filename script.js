f7e44c98b3632971f336a221e97c7294ffa0ce71
Anant <anant13u@gmail.com>
2023-10-18 16:49:09 +0530

    Updated the code to use a more descriptive class name for video elements. Also, introduced a function to extract video ID from the URL. Now, the embedded video will autoplay upon selection.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const listVideo = document.querySelectorAll('.list-video')
+const listVideo = document.querySelectorAll('.list-item')
 const recipeVideo = document.getElementById('recipe-video')
 
 listVideo.forEach(video => {
@@ -7,16 +7,24 @@ listVideo.forEach(video => {
 
 async function fetchVideo(event) {
     event.preventDefault()
-    
-    const embedUrl = `${event.target.href}`;
+
+    const videoID = getVideoID(event.target.href);
+    const embedUrl = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
+    // console.log('embedUrl is: ' + event.target.href)
     // console.log(`Embed URL is ${thumbnailUrl1}`)
     // const videoUrl2 = `https://www.youtube.com/watch?v=${videoID2}`
     // console.log(`The Video URL is ${videoUrl2}`)
     
     const iframeHtml = `<iframe width="560" height="315" src="${embedUrl}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
-    const iframeHtml = `<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_HERE" frameborder="0" allowfullscreen></iframe>
+    // const iframeHtml = `<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_HERE" frameborder="0" allowfullscreen></iframe>
 
     console.log(`iFrameHtml is ${iframeHtml}`)
     
     recipeVideo.innerHTML = iframeHtml
 }
+
+function getVideoID(url) {
+    const urlParams = new URLSearchParams(new URL(url).search);
+    return urlParams.get('v');
+}
+
