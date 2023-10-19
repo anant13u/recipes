1dfe81ed90b130a61f2d5926726ab69b35b7770c
Anant <anant13u@gmail.com>
2023-10-19 21:46:55 +0530

    Added comments to explain how URLSearchParams works.

--- script.js
+++ script.js
@@ -12,19 +12,18 @@ async function fetchVideo(event) {
     const embedUrl = `https://www.youtube.com/embed/${videoID}?autoplay=1`;
     // console.log('embedUrl is: ' + event.target.href)
     // console.log(`Embed URL is ${thumbnailUrl1}`)
-    // const videoUrl2 = `https://www.youtube.com/watch?v=${videoID2}`
-    // console.log(`The Video URL is ${videoUrl2}`)
     
     const iframeHtml = `<iframe width="560" height="315" src="${embedUrl}" frameborder="0" style='border-radius:15px' allowfullscreen></iframe>`;
     // const iframeHtml = `<iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID_HERE" frameborder="0" allowfullscreen></iframe>
 
     console.log(`iFrameHtml is ${iframeHtml}`)
-    
     recipeVideo.innerHTML = iframeHtml
 }
 
 function getVideoID(url) {
+    // Create a new URLSearchParams object by passing the search portion of the URL as an argument
     const urlParams = new URLSearchParams(new URL(url).search);
+    // Use the get method of URLSearchParams to retrieve the value associated with the key 'v'
+    // In the context of YouTube URLs, 'v' typically represents the video ID
     return urlParams.get('v');
 }
-
